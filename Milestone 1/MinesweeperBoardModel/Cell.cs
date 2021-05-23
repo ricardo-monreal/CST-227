970704155434581dc5812ac8d3171d3bc8bb557d
ricardo-monreal <moises.monreal@gmail.com>
2021-05-23 10:42:01 -0700

    Milestone 1
    Created Cell and Board class

---
+++ Milestone 1/MinesweeperBoardModel/Cell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperBoardModel
+{
+    class Cell
+    {
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+
+        public bool Visited { get; set; }
+        public bool Live { get; set; }
+
+        public Cell()
+        {
+            RowNumber = -1;
+            ColumnNumber = -1;
+            Visited = false;
+            Live = false;
+        }
+
+        // Cell constructor
+        public Cell(int x, int y)
+        {
+            RowNumber = x;
+            ColumnNumber = y;
+        }
+
+
+    }
+}
