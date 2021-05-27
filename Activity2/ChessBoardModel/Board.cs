cc04318a12e87780a28cc1802cf2910d84f25e07
ricardo-monreal <moises.monreal@gmail.com>
2021-05-26 21:03:22 -0700

    Created class library
    Cell and Board classes

---
+++ Activity2/ChessBoardModel/Board.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardModel
+{
+    class Board
+    {
+        // set the size of the board
+        public int Size { get; set; }
+
+        //2d array of type cell
+        public Cell[,] theGrid { get; set; }
+
+        // constructor
+        public Board (int s)
+        {
+            // initial size of the board is defined by s.
+            Size = s;
+
+            // create a new 2d array of type cell.
+            theGrid = new Cell[Size, Size];
+
+            // fill the 2d array with new Cells, each with unique x and y coordinates.
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+
+
+        public void MarkNextLegalMoves(Cell currentCell, string chessPiece)
+        {
+            // clear all previous legal moves
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+
+            // find all legal moves and mark the cell as legal moves
+            switch (chessPiece)
+            {
+                case "Knight":
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber -1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+
+                    break;
+                case "King":
+                    break;
+                case "Rook":
+                    break;
+                case "Bishop":
+                    break;
+                case "Queen":
+                    break;
+
+                default:
+                    break;
+
+
+            }
+        }
+    }
+}
