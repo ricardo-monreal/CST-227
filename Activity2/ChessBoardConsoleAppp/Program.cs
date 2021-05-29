e51f46b2496b6811fd1904a531068d09f629af40
ricardo-monreal <moises.monreal@gmail.com>
2021-05-29 16:43:29 -0700

    Updated Board class
    Missing boundary check for King and Knight

--- Activity2/ChessBoardConsoleAppp/Program.cs
+++ Activity2/ChessBoardConsoleAppp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessBoardModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +10,133 @@ namespace ChessBoardConsoleAppp
 {
     class Program
     {
+        static Board myBoard = new Board(8);
+
         static void Main(string[] args)
         {
 
-            Board myBoard = new Board(8);
             // show the empty board
+            printBoard(myBoard);
+
+           
+            // ask the user for input. x and y coordinates where to place a piece
+            Cell currentCell = setCurrentCell();
+            string chesspice = setCurrentChessPiece();
+            currentCell.CurrentlyOccupied = true;
+
+            // calculate all legal moves for that pice
+            myBoard.MarkNextLegalMoves(currentCell, chesspice);
+
+           
+            // print the chess board. X is for occupied square.
+            printBoard(myBoard);
+
+
+
+            // end program with returned key
+            Console.ReadLine();
+
+        }
+        // Challenge - set multiple chess pieces
+        public static string setCurrentChessPiece()
+        {
+            string choice;
+            
+
+            do
+            {
+                Console.WriteLine("Please select a chess piece\n");
+                Console.WriteLine("a - Knight\nb - King\nc - Rook\nd - Bishop\ne - Queen\n");
+                choice = Console.ReadLine();
+
+            } while (choice != "a" && choice != "b" && choice != "c" && choice != "d" && choice != "e");
+
+                if (choice.ToLower() == "a")
+                {
+                    choice = "Knight";
+                }
+                else if (choice.ToLower() == "b")
+                {
+                    choice = "King";
+                }
+                else if (choice.ToLower() == "c")
+                {
+                    choice = "Rook";
+                }
+                else if (choice.ToLower() == "d")
+                {
+                    choice = "Bishop";
+                }
+                else if (choice.ToLower() == "e")
+                {
+                    choice = "Queen";
+                }
+                else
+                {
+                    Console.WriteLine("Please select a valid option");
+                }
+            return choice;
 
 
         }
+
+       
+
+        private static Cell setCurrentCell()
+        {
+            // get x and y coordinates from the user. return a cell location
+            Console.WriteLine("Enter the current row number");
+            int currentRow = UserIntInput();
+
+            Console.WriteLine("Enter the current column number");
+            int currentColumn = UserIntInput();
+
+            return myBoard.theGrid[currentRow, currentColumn];
+
+        }
+
+
+
+
+        // print chess board to the console
+        static void printBoard(Board myBoard)
+        {
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    Cell c = myBoard.theGrid[i, j];
+
+                    if (c.CurrentlyOccupied == true)
+                    {
+                        Console.Write("x");
+                    }
+                    else if (c.LegalNextMove == true)
+                    {
+                        Console.Write("+");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("=================================");
+        }
+
+
+        // error checking method
+        public static int UserIntInput()
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Please input an integer number");
+            }
+
+            return choice;
+        }
+        
     }
 }
---
+++ Activity2/ClassLibrary1/Board.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardModel
+{
+    public class Board
+    {
+        // set the size of the board
+        public int Size { get; set; }
+
+        //2d array of type cell
+        public Cell[,] theGrid { get; set; }
+
+        // constructor
+        public Board (int s)
+        {
+            // initial size of the board is defined by s.
+            Size = s;
+
+            // create a new 2d array of type cell.
+            theGrid = new Cell[Size, Size];
+
+            // fill the 2d array with new Cells, each with unique x and y coordinates.
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+
+
+        public void MarkNextLegalMoves(Cell currentCell, string chessPiece)
+        {
+            // clear all previous legal moves
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+
+            // find all legal moves and mark the cell as legal moves
+            switch (chessPiece)
+            {
+                case "Knight":
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber -1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    break;
+
+                case "King":
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    break;
+
+                case "Rook":
+                    CheckValidRookSpaces(currentCell);
+                    break;
+
+                case "Bishop":
+                    CheckValidBishopSpaces(currentCell);
+                    break;
+
+                case "Queen":
+                    CheckValidBishopSpaces(currentCell);
+                    CheckValidRookSpaces(currentCell);
+                    break;
+
+                default:
+                    break;
+            }
+            theGrid[currentCell.RowNumber, currentCell.ColumnNumber].CurrentlyOccupied = true;
+
+        }
+
+        // check if input is within the grid values
+        public bool CheckGridSpace(int r, int c)
+        {
+            return (r < theGrid.GetLength(0) && r >= 0) && (c < theGrid.GetLength(1) && c >= 0);
+        }
+
+
+        public void CheckValidRookSpaces(Cell current)
+        {
+            int column = current.ColumnNumber;
+            int row = current.RowNumber;
+            //check right
+            for (int i = column; i < Size; i++)
+            {
+                if (CheckGridSpace(row, i))
+                {
+                    theGrid[row, i].LegalNextMove = true;
+                }
+            }
+            // check left
+            for (int i = column; i >= 0; i--)
+            {
+                if (CheckGridSpace(row, i))
+                {
+                    theGrid[row, i].LegalNextMove = true;
+                }
+            }
+            // check top
+            for (int i = row; i >= 0; i--)
+            {
+                if (CheckGridSpace(row, i))
+                {
+                    theGrid[i, column].LegalNextMove = true;
+                }
+            }
+            // check bottom
+            for (int i = row; i < Size; i++)
+            {
+                if (CheckGridSpace(row, i)) theGrid[i, column].LegalNextMove = true;
+            }
+        }
+
+        // check for Bishop 
+        public void CheckValidBishopSpaces(Cell current)
+        {
+            int column = current.ColumnNumber;
+            int row = current.RowNumber;
+
+            // check top right
+            for (int i = row; i >= 0; i--)
+            {
+                if (CheckGridSpace(i, column + (current.RowNumber -i)))
+                {
+                    theGrid[i, column + (current.RowNumber - i)].LegalNextMove = true;
+                }
+            }
+            // check top left
+            for (int i = row; i >= 0; i--)
+            {
+                if (CheckGridSpace(i, column - (current.RowNumber - i)))
+                {
+                    theGrid[i, column - (current.RowNumber - i)].LegalNextMove = true;
+                }
+            }
+            // check bottom right
+            for (int i = row; i < Size; i++)
+            {
+                if (CheckGridSpace(i, column + (current.RowNumber - i)))
+                {
+                    theGrid[i, column + (current.RowNumber - i)].LegalNextMove = true;
+                }
+            }
+            // check bottom left
+            for (int i = row; i < Size; i++)
+            {
+                if (CheckGridSpace(i,  - (current.RowNumber - i)))
+                {
+                    theGrid[i, column - (current.RowNumber - i)].LegalNextMove = true;
+                }
+            }
+
+        }
+
+
+    }
+}
