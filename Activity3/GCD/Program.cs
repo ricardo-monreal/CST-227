2798867d78c6496e6b841563d1c9b5a3a93ca4ed
ricardo-monreal <moises.monreal@gmail.com>
2021-06-06 10:26:04 -0700

    Update Activity 3

--- Activity3/GCD/Program.cs
+++ Activity3/GCD/Program.cs
@@ -8,19 +8,21 @@ namespace GCD
         {
             int number1 = 400;
             int number2 = 85;
+            Console.WriteLine($"Calculating the GCD for numbers {number1} and {number2}. \n");
             int answer = gcd(number1, number2);
-            Console.WriteLine("the gcd of {0} and {1} is {2}", number1, number2, answer);
+            Console.WriteLine("\nThe GCD of {0} and {1} is {2}.", number1, number2, answer);
         }
 
         static int gcd(int n1, int n2)
         {
+
             if (n2 == 0)
             {
                 return n1;
             }
             else
             {
-                Console.WriteLine("Not yet. Remainder is {0}", n1 % n2);
+                Console.WriteLine("Calculating...\n Remainder is {0}", n1 % n2);
                 return gcd(n2, n1 % n2);
             }
         }
