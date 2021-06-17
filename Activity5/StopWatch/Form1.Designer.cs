a53ec6f9db67d30967459bffacb4662d90e491cc
ricardo-monreal <moises.monreal@gmail.com>
2021-06-16 22:21:21 -0700

    Activity 5
    Base complete, needs challenges

---
+++ Activity5/StopWatch/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace StopWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label_timer = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_target = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_timer
+            // 
+            this.label_timer.AutoSize = true;
+            this.label_timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_timer.Location = new System.Drawing.Point(134, 29);
+            this.label_timer.Name = "label_timer";
+            this.label_timer.Size = new System.Drawing.Size(0, 29);
+            this.label_timer.TabIndex = 0;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(41, 341);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(171, 341);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 2;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(299, 341);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 3;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_target
+            // 
+            this.btn_target.BackColor = System.Drawing.Color.White;
+            this.btn_target.BackgroundImage = global::StopWatch.Properties.Resources.mole;
+            this.btn_target.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_target.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_target.Location = new System.Drawing.Point(194, 169);
+            this.btn_target.Name = "btn_target";
+            this.btn_target.Size = new System.Drawing.Size(93, 74);
+            this.btn_target.TabIndex = 4;
+            this.btn_target.UseVisualStyleBackColor = false;
+            this.btn_target.Click += new System.EventHandler(this.btn_target_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::StopWatch.Properties.Resources.grass_background;
+            this.ClientSize = new System.Drawing.Size(491, 451);
+            this.Controls.Add(this.btn_target);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.label_timer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_timer;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_target;
+    }
+}
+
---
+++ Activity5/StopWatch/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+   
+
+    public partial class Form1 : Form
+    {
+
+        public static Stopwatch watch = new Stopwatch();
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            watch.Stop();
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            watch.Reset();
+        }
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //label_timer.Text = watch.Elapsed.ToString();
+
+            
+            
+            label_timer.Text = watch.Elapsed.ToString();
+
+
+          
+
+            if (random.Next(0,10) < 5)
+            {
+                btn_target.Left = random.Next(0, this.Width);
+                btn_target.Top = random.Next(0, this.Height);
+                btn_target.Visible = true;
+            }
+        }
+
+        private void btn_target_Click(object sender, EventArgs e)
+        {
+            btn_target.Visible = false;
+        }
+    }
+}
